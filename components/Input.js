679e9f3e0de30b8eda3479c476ff0559720faffa
Victor Wu <victorwu@Victors-MacBook-Air.local>
2023-01-24 14:51:44 -0800

    confirm button and reset text

--- components/Input.js
+++ components/Input.js
@@ -1,17 +1,25 @@
-import { View, Text, TextInput } from 'react-native'
-import React from 'react'
-import { useState } from 'react'
+import { View, Text, TextInput, Button } from "react-native";
+import React from "react";
+import { useState } from "react";
 
-export default function Input({textUpdateFunction}) {
-    const [text, setText] = useState('');
-    return (
-        <View>
-        <TextInput 
-        style = {{backgroundColor: "red"}}
+export default function Input({ textUpdateFunction }) {
+  const [text, setText] = useState("");
+  return (
+    <View>
+      <TextInput
+        style={{ backgroundColor: "red" }}
         value={text}
         onChangeText={(changedText) => {
-            textUpdateFunction(changedText)
-            setText(changedText)}}/>
+          setText(changedText);
+        }}
+      />
+      <Button
+        title="Confirm"
+        onPress={() => {
+          textUpdateFunction(text);
+          setText("");
+        }}
+      />
     </View>
-  )
+  );
 }
