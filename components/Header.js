eac2e8f849edeba63c69f2bd7beb52a2b5c24350
Victor Wu <victorwu@Victors-Air.hitronhub.home>
2023-01-27 12:21:36 -0800

    comment out console log

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function Header({appName}) {
-    //console.log(props)
+export default function Header({ appName }) {
+  //console.log(props)
   return (
     <View>
-      <Text style = {{borderColor: 'purple', borderWidth: '1', padding: 5, margin: 5, color: 'purple', fontWeight: 'bold'}}>Welcome to {appName}</Text>
+      <Text
+        style={{
+          borderColor: "purple",
+          borderWidth: "1",
+          padding: 5,
+          margin: 5,
+          color: "purple",
+          fontWeight: "bold",
+        }}
+      >
+        Welcome to {appName}
+      </Text>
     </View>
-  )
+  );
 }
