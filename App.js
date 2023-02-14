d128a4cbf01fbb36c54c800a35284dee9694c36a
Victor Wu <victorwu@victors-air.nunet.neu.edu>
2023-02-14 15:49:45 -0800

    add styling to home page

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Home} options={{headerStyle: {backgroundColor: 'purple'}, headerTintColor: 'pink' }} />
         <Stack.Screen name="Details" component={GoalDetails} />
       </Stack.Navigator>
     </NavigationContainer>
