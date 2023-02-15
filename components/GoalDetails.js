c528a6b3bf2f9a91d5b10cc87bae5b9f63afde66
Victor Wu <victorwu@victors-air.nunet.neu.edu>
2023-02-14 16:27:48 -0800

    commenting what setOptions does

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,18 +1,23 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
-export default function GoalDetails({route}) {
-    const navigation = useNavigation()
+export default function GoalDetails({ route }) {
+  const navigation = useNavigation();
+
+  //you can use navigation.setOptions here to set navigation options for 
+  //the screen but then use it to call functions within the component screen
   return (
     <View>
       <Text>{route.params.item.text}</Text>
       <Button
-      title = "more details"
-      onPress= {() => {
-        navigation.push("Details", {item: {id: "", text: "this is a new goal"}})
-      }}
-       />
+        title="more details"
+        onPress={() => {
+          navigation.push("Details", {
+            item: { id: "", text: "this is a new goal" },
+          });
+        }}
+      />
     </View>
-  )
+  );
 }
