b947f3e94f08dd0aafe68603ad8373b29d8299e6
Victor Wu <victorwu@Victors-MacBook-Air.local>
2023-02-21 14:43:57 -0800

    adding firebase

---
+++ Firebase/firebase-setup.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8_oNk1yK3XK-o_PYEf2em4F9y_6rAFZQ",
+  authDomain: "cs5520-82661.firebaseapp.com",
+  projectId: "cs5520-82661",
+  storageBucket: "cs5520-82661.appspot.com",
+  messagingSenderId: "174414441995",
+  appId: "1:174414441995:web:b34fa1469c60352eb8610e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
