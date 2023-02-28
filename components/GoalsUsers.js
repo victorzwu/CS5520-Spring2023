5d5435d79fa0ce3a580f443ab83da7ca46a297f4
Victor Wu <victorwu@Victors-Air.attlocal.net>
2023-02-28 15:00:24 -0800

    add GoalsUsers

---
+++ components/GoalsUsers.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+
+export default function GoalsUsers() {
+    useEffect(()=>{
+        //get data here and update some state variables
+    }, [])
+  return (
+    <View>
+      <Text>GoalsUsers</Text>
+    </View>
+  )
+}
