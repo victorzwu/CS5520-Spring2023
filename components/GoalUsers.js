bcc08f879091b4ca90a6beeebd61ae194798d6e8
Victor Wu <victorwu@Victors-Air.attlocal.net>
2023-02-28 16:16:21 -0800

    use Dimension width and height

--- components/GoalUsers.js
+++ components/GoalUsers.js
@@ -1,9 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 
 export default function GoalUsers() {
-  const[users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     //get data here and update some state variables
@@ -12,28 +12,51 @@ export default function GoalUsers() {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/users"
         );
-        if(!response.ok)
-        {
-            throw new Error("HTTP error happened");
+        if (!response.ok) {
+          throw new Error("HTTP error happened with status: ", response.status);
         }
+        //in json everything is a string
         const data = await response.json();
-        const nameArray = data.map((userData)=>{return userData.name})
+        const nameArray = data.map((userData) => {
+          return userData.name;
+        });
         setUsers(nameArray);
-        console.log(nameArray)
+        console.log(nameArray);
       } catch (err) {
         console.log("get Users error:", err);
       }
     }
     getUsers();
   }, []);
+
+  async function addUser() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ name: "Victor", id: Math.random() }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("HTTP error happened with status", response.status);
+      }
+      const data = await response.json();
+      setUsers((prevUser) => [...prevUser, data.name]);
+    } catch (err) {
+      console.log("get Users error:", err);
+    }
+  }
   return (
     <View>
-      <FlatList data={users}
-          renderItem={({ item }) => {
-            return (
-              <Text>{item}</Text>
-            );
-          }}/>
+      <FlatList
+        data={users}
+        renderItem={(userData) => {
+          return <Text>{userData.item}</Text>;
+        }}
+      />
+      <Button title="Add me as a user" onPress={addUser} />
     </View>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 import React from "react";
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default function Header({ appName }) {
   //console.log(props)
@@ -11,8 +14,12 @@ export default function Header({ appName }) {
           borderWidth: 1,
           padding: 5,
           margin: 5,
+          fontSize: windowWidth>380? 36: 24,
           color: "purple",
           fontWeight: "bold",
+          width: 350,
+          maxWidth: "80%",
+          
         }}
       >
         Welcome to {appName}
